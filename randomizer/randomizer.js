e7d7175bd3866dd55d20fa02e4174132a3da59e7
Utsuno Taishiro <t.utsuno@d-head.co.jp>
2016-04-12 19:44:38 +0900

    randomize.js updated

--- randomizer/randomizer.js
+++ randomizer/randomizer.js
@@ -1,4 +1,4 @@
-// Randomizer ver.1.0.1 data:2016-04-12
+// Randomizer ver.1.0.2 data:2016-04-12
 $(function(){
   function random(array, num) {
     var a = array;
@@ -14,13 +14,18 @@ $(function(){
     }
     return r;
   }
-  var test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 23, 25, 26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63];
+  var arrayEC = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  var arrayTD = [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 61, 62];
+  var arrayOT = [16, 18, 20, 21, 23, 25, 26, 27, 28, 30, 31, 32, 33, 34, 35, 48, 49, 50, 51, 53, 54, 55, 56, 57, 58, 59, 60, 63];
   var $results = $('#result');
   $('#randomize').on('click', function() {
     if ( $('#result li').length ) $('#result li').remove();
-    var four = random(test_array, 4);
-    for (var i = four.length - 1; i >= 0; i--) {
-      $results.append('<li>' + four[i] + '</li>');
+    var memberEC = random(arrayEC, 1)
+      , memberTD = random(arrayTD, 1)
+      , memberOT = random(arrayOT, 2);
+    $results.append('<li>'+ memberEC +'</li>').append('<li>'+ memberTD +'</li>');
+    for (var i = memberOT.length - 1; i >= 0; i--) {
+      $results.append('<li>' + memberOT[i] + '</li>');
     }
   });
 })
